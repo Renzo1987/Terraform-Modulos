a95203b712ba4e74897177d344be330d00cb1ccd
Renzo1987 <145566039+Renzo1987@users.noreply.github.com>
2023-11-23 14:14:33 +0100

    prueba

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/   ')
 def hola():
     return 'Bienvenidos'
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0")
+    app.run_server(host="0.0.0.0", port=8080, debug=True)
