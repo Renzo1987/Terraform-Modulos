4d9d423a35f74d0bd341c553af5e3257ceadc63b
Renzo1987 <145566039+Renzo1987@users.noreply.github.com>
2023-11-27 16:50:44 +0100

    primer commit

--- main.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/   ')
-def hola():
-    return 'Bienvenidos'
-
-if __name__ == '__main__':
-    app.run_server(host="0.0.0.0", port=8080, debug=True)
